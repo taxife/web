03d09c8f7d1d5bde5d9a092e46001f2fb4625112
Jose <jmartinez@openfinance.es>
2013-05-22 21:48:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taxife::Application.config.secret_token = '737a7a410309927afbd691a61c36fbe1036f00b8691f9ea7bca6fe4be21a1b76c388cc9fd6ea12ee30d0239784f7a3a3fc345d400e16d18ad6a93bc5b4eb7f3a'
