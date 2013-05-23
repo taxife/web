0887ff40ff69f603189a1ea4a684d5ea045a2ea8
Jose <jmartinez@openfinance.es>
2013-05-23 07:55:16 +0200

    tuktuk css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Taxife::Application.routes.draw do
+
+  match '/home',     :to => "pages#home"
+
+  get "pages/home"
+
+  root :to => "pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
